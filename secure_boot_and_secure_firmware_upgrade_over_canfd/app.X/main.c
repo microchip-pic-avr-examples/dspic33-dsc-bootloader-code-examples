e2e6fe97592fa705743baed5a7747b0b6ba8bef5
Anastasia Wyatt <Tess.Wyatt@microchip.com>
2024-03-20 12:45:17 -0700

    Pull functionality out into functions, mask upper 16 bits of opcode, and add information about the GOTO instruction

--- secure_boot_and_secure_firmware_upgrade_over_canfd/app.X/main.c
+++ secure_boot_and_secure_firmware_upgrade_over_canfd/app.X/main.c
@@ -19,7 +19,6 @@
     THIS SOFTWARE.
  */
 #include "mcc_generated_files/system/system.h"
-#include "mcc_generated_files/system/pins.h"
 #include "mcc_generated_files/flash/flash.h"
 #include "mcc_generated_files/flash/flash_types.h"
 #include "mcc_generated_files/boot/boot_config.h"
@@ -28,20 +27,42 @@
     Main application
  */
 
+/* The following code finds the address used by GOTO instruction programmed 
+ * at the reset vector in order to determine whether or not the application 
+ * was downloaded directly or via the bootloader (i.e. if the address within the 
+ * GOTO instruction is within the bootloader or application), as the ICSP 
+ * inhibit functionality is only permitted if the application was 
+ * downloaded via the bootloader.
+ * 
+ * The GOTO instruction contains two 24 bit words, the first of which contains 
+ * the lower 15 bits of the address  and the second of which contains the upper 
+ * 7 bits. The following code reads the GOTO address at the reset vector and 
+ * parses the two words in order to determine the reset address. 
+ * 
+ * Refer to https://ww1.microchip.com/downloads/en/DeviceDoc/70000157g.pdf for 
+ * additional details on the GOTO instruction format.   
+ */
+uint32_t GetResetAddress()
+{   
+    flash_data_t flashData[2];
+    FLASH_Read(0x000000, 2, flashData);
+    return (flashData[1]<<16) | flashData[0];
+}
+
+bool WasLoadedByBootloader()
+{
+    return ((GetResetAddress() & 0x0000FFFF) < BOOT_CONFIG_PROGRAMMABLE_ADDRESS_LOW);
+}
+
 int main(void) {
 
     SYSTEM_Initialize();
     
-    flash_data_t flashData[2];
-    FLASH_Read(0x000000, 2, flashData);
-    uint16_t resetAddressLower = flashData[0];
-    uint32_t combinedResetAddress = (flashData[1]<<16) | resetAddressLower;
-    
     while (1) 
     {
-        if (combinedResetAddress < BOOT_CONFIG_PROGRAMMABLE_ADDRESS_LOW) 
+        if (WasLoadedByBootloader()) 
         {
-            // ICSP Feature
+            // ICSP Inhibit
         }
     }
 }
